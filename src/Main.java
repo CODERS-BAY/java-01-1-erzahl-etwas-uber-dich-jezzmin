53b2f8ec9f943bb077dcd569f887d7d24fc1f95b
Jasmin Gelbmann <jasmin.gelbmann@gmail.com>
2021-02-11 10:53:01 +0100

    add exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+
+        int age = 21;
+        String firstName = "Jasmin";
+        char gender = 'f';
+        Date birthDay = new Date (947026800000L);
+        double grade = 1.6;
+        boolean married = false;
+
+        System.out.println("Hello my name is " + firstName + ". I am " + age + " years old and my birthday is " + birthDay + " ,my grade was " + grade + ". I am " + gender + " and am i married? " + married + ".");
+
+
     }
 }
